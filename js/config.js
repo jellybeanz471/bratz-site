cfec4de7292bd1c089dcec78505fa621d758bbd2
jellybeanz471 <ayymiguel.miguel@gmail.com>
2024-11-02 20:18:45 -0400

    Update config.js
    update userpool id and client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_dein8i9Qa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4idu5jnqorgfkohlfiu00gu7f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_Fkye1JkX8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'gfkmasr3nkjrofb4v40k7c1ch', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
